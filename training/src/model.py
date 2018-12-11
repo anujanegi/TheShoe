197fc5fa3bcb79a78364c05cfb8b15cf824d2504
anujanegi <anujanegi1@gmail.com>
2018-12-11 15:14:55 +0530

    Restructured dir

---
+++ training/src/model.py
@@ -0,0 +1,32 @@
+import numpy as np
+import tensorflow as tf
+from keras.datasets import reuters
+from keras.utils.np_utils import to_categorical
+from keras.preprocessing.text import Tokenizer
+from keras import models
+from keras import layers
+
+
+def def_model(num_of_features) :
+
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Dense(100, activation='relu', input_shape=(num_of_features, )))
+    model.add(tf.keras.layers.Dense(70, activation='relu'))
+    model.add(tf.keras.layers.Dense(50, activation='relu'))
+    model.add(tf.keras.layers.Dense(30, activation='relu'))
+    model.add(tf.keras.layers.Dense(5, activation='softmax'))
+    return model
+
+def compile_model(model):
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+    return model
+
+def fit_model(model, trainX, trainY, testX, testY):
+    # model.fit(np.array(trainX), np.array(trainY), epochs=200, verbose=1, batch_size=10, validation_data=(np.array(testX), np.array(testY)))
+    model.fit(trainX, trainY, epochs=200, verbose=1, batch_size=10, validation_data=(testX, testY))
+
+    return model
+
+def evaluate(model, X, Y):
+	scores = model.evaluate(X,Y)
+	print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
---
+++ training/src/train.py
@@ -0,0 +1,18 @@
+import numpy as np
+import pickle
+import tensorflow as tf
+from keras.models import load_model
+from data import *
+from model import *
+
+np.random.seed(399)
+trainX, trainY, testX, testY = load_data()
+# print(trainX.shape, trainY.shape)
+model = fit_model(compile_model(def_model(6)), trainX, trainY, testX, testY)
+evaluate(model, trainX, trainY)
+
+filename = "../../models/user1/model.h5"
+tf.keras.models.save_model(model, filename)
+converter = tf.contrib.lite.TocoConverter.from_keras_model_file(filename)
+tflite_model = converter.convert()
+open("./Models/user1/modeltest.tflite", "wb").write(tflite_model)
